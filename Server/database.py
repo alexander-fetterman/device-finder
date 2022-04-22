c035304da426dea4a28e5b5d2b86df2f63a88068
alexander-fetterman <afyankeefan@gmail.com>
2022-04-22 18:33:23 -0400

    Adding server files

---
+++ Server/database.py
@@ -0,0 +1,105 @@
+######################################################################
+## IMPORTS
+######################################################################
+from configparser import ConfigParser
+import psycopg2 as psql
+
+######################################################################
+## CLASSES
+######################################################################
+
+class Database:
+    ######################################################################
+    ## CONSTANTS
+    ######################################################################
+    
+    DATABASE_TYPE = 'postgresql'
+    TABLE_NAME = 'sensordata'
+
+    ######################################################################
+    ## MEMBER VARIABLES
+    ######################################################################
+    
+
+    ######################################################################
+    ## CONSTRUCTORS
+    ######################################################################
+    
+    '''
+    Creates a Database object. Does not connect to any database. Defines
+     all member variables. Set the connection member variable to null.
+    @param filename --- The initialization filename which will be used for connection
+    '''
+    def __init__( self, filename ):
+        self.FILENAME = filename
+
+    ######################################################################
+    ## PRIVATE METHODS
+    ######################################################################
+
+    '''
+    Parses the internally stored config file, and returns the parameters
+      necessary to connect to a database.
+    '''
+    def __parse_config( self ):
+        # create a parser
+        parser = ConfigParser()
+        
+        # read config file
+        parser.read( self.FILENAME )
+
+        # get section, default to postgresql
+        db = {}
+        if parser.has_section( self.DATABASE_TYPE ):
+            params = parser.items( self.DATABASE_TYPE )
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format( self.DATABASE_TYPE, self.FILENAME ))
+
+        return db
+    
+    '''
+    Builds the string representation of the query to insert 
+    '''
+    def build_insert_query():
+        raise Exception( 'Method not yet implemented' )
+
+    ######################################################################
+    ## PUBLIC METHODS
+    ######################################################################
+
+
+    '''
+    Connects to a database and stores the connection within this 
+     object. This is required before calling any DB operations.
+    '''
+    def db_connect( self ):
+        # Connect to the database using the config file
+        self.connection = psql.connect( **self.__parse_config() )
+
+        # Open a cursor to the database
+        self.cursor = self.connection.cursor()
+
+    '''
+    Inserts a tuple into the database
+    '''
+    def insert_values( self, timestamp, moisture_level ):
+        # Perform the insert operation
+        self.cursor.execute( "INSERT INTO {table} VALUES( {ts}, {ms} )".format( table = self.TABLE_NAME, ts = timestamp, ms = moisture_level ) )
+        
+        # Commit the insert operation
+        self.connection.commit()
+
+    '''
+    Get the most recent tuples from the database.
+    @param limit --- The maximum number of tuples to return.
+    '''
+    def get_values( self, limit ):
+        # Execute a query - this does not return any data yet
+        self.cursor.execute( "SELECT * FROM sensordata order by ts desc LIMIT {lm};".format( table = self.TABLE_NAME, lm=limit ) )
+
+        # Fetch all of the data requested. This may be a time intensive 
+        #  operation over external network.
+        tuples = self.cursor.fetchall()
+        return tuples
---
+++ Server/server.py
@@ -0,0 +1,126 @@
+######################################################################
+## IMPORTS
+######################################################################
+
+from socket import MSG_ERRQUEUE
+import paho.mqtt.client as paho
+import json
+import threading
+
+######################################################################
+## CLASSES
+######################################################################
+
+class MqttDriver:
+    ######################################################################
+    ## CONSTANTS
+    ######################################################################
+
+    BROKER_URI = "127.0.0.1"
+    CLIENT_NAME = "DemoClient"
+    TOPIC = "/Demo"
+
+    ######################################################################
+    ## CLASS VARIABLES
+    ######################################################################
+
+    CLIENT = None
+
+    ######################################################################
+    ## MEMBER VARIABLES
+    ######################################################################
+
+
+    ######################################################################
+    ## CONSTRUCTORS
+    ######################################################################
+
+    '''
+    Creates the member client. THIS METHOD DOES NOT DO ANY CONNECTING
+     OR SUBSCRIBING!
+    @param clientName --- The (unique) client name to be assigned to the
+     member client.
+    @param database --- A (user defined) database object, supplied externally
+    '''
+    def initialize( ): #database ):
+        # Create a static MQTT client
+        MqttDriver.CLIENT = paho.Client( MqttDriver.CLIENT_NAME )
+
+        # Set the callback method for message received
+        MqttDriver.CLIENT.on_message = MqttDriver.__message_received
+
+        # Use the argument database as the member database
+        # self.database = database
+
+
+    ######################################################################
+    ## PRIVATE CALLBACKS
+    ######################################################################
+
+    '''
+    Acts as a driver for all logic following a message being received.
+    '''
+    def __message_received( client, userdata, message ):
+        print( "Message received: ", str( message.payload.decode("utf-8") ) )
+        
+        # Parse the message into a dictionary
+        msg_values = json.loads( str( message.payload.decode("utf-8") ) )
+        # print( "Timestamp: {ts}".format( ts = msg_values[ 'ts' ] ) )
+        # print( "Moisture Level: {ms}".format( ms = msg_values[ 'ms' ] ) )
+
+        #print( msg_values )
+        print( "Device detected in room %d at time %d" % ( msg_values[ 'dev' ], msg_values[ 'ts' ] ) )
+
+        # Insert the tuple into the database
+        # print( 'Inserting tuple...' )
+        # self.database.insert_values( msg_values[ 'ts' ], msg_values[ 'ms' ] )
+
+
+    ######################################################################
+    ## PRIVATE METHODS
+    ######################################################################
+
+
+    ######################################################################
+    ## PUBLIC METHODS
+    ######################################################################
+    
+    '''
+    Connects the member Mqtt Client to the remote broker
+    @param brokerURI --- The URI (universal resource identifier) of the broker
+     to connect to. This should be in the form specified by the paho library.
+    '''
+    def connect(  ):
+        # Connect the member client to the broker at the parameter
+        #  specified URI
+        MqttDriver.CLIENT.connect( MqttDriver.BROKER_URI )
+    
+    '''
+    Subscribes the member Mqtt Client to a given topic
+    @param topic --- The topic to subscribe to
+    '''
+    def subscribe(  ):
+        # Subcribe to the input topic
+        MqttDriver.CLIENT.subscribe( MqttDriver.TOPIC )
+
+    '''
+    Runs the client loop. This must be called after the connect method
+     has been called.
+    '''
+    def run(  ):
+        print( "B" )
+
+        # Inititialze the class
+        if MqttDriver.CLIENT is None:
+            MqttDriver.initialize()
+            MqttDriver.connect()
+            MqttDriver.subscribe()
+
+        print( "A" )
+
+        # Start the client loop - use loop forever because we do not
+        #  want to return.
+        MqttDriver.CLIENT.loop_forever()
+
+
+MqttDriver.run()
